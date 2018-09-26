cfb0e6b8772054e9bc4cc53ef3e76f0139e9a338
buxizhizhoum <mapeaks@126.com>
2018-09-26 22:09:10 +0800

    Reorgnize

--- top_n/top_n_df.py
+++ top_n/top_n_df.py
@@ -26,12 +26,16 @@ sc = spark.sparkContext
 if __name__ == "__main__":
     text_rdd = sc.textFile("../data/word_count.txt")
     print(text_rdd.take(10))
-    words_count = text_rdd.map(lambda line: line.rstrip("!,.")).flatMap(lambda line: line.split()).map(lambda word: (word, 1))
+    words_count = text_rdd.map(lambda line: line.rstrip("!,."))\
+                          .flatMap(lambda line: line.split())\
+                          .map(lambda word: (word, 1))
     print(words_count.take(10))
 
     # convert to dataframe after flatMap
     text_df = spark.createDataFrame(words_count, ["words", "count"])
     text_df.printSchema()
     print(text_df.show(10))
-    group_by = text_df.groupBy("words").sum().orderBy("sum(count)", ascending=False).collect()
-    print(group_by)
+    group_by = text_df.groupBy("words")\
+                      .sum()\
+                      .orderBy("sum(count)", ascending=False)
+    print(group_by.collect())
