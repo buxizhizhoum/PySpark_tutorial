e1e4159d1b62e6332abf3e6ad008f79d3eed6abb
buxizhizhoum <mapeaks@126.com>
2018-09-26 17:46:19 +0800

    Add top_n.py

---
+++ top_n/top_n.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+get the top n words after word count
+"""
+from __future__ import print_function
+import os
+
+from pyspark import SparkContext
+from pyspark import SparkConf
+
+
+os.environ["PYSPARK_PYTHON"] = "/usr/bin/python3"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "/usr/bin/python3"
+os.environ['PYSPARK_SUBMIT_ARGS'] = \
+    '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 ' \
+    'pyspark-shell'
+
+TOP_N = 10
+
+conf = SparkConf().setAppName("TopK").setMaster("local[2]")
+sc = SparkContext(conf=conf)
+
+
+def word_count(rdd):
+    pre_process_rdd = rdd.map(lambda line: line.strip("!,."))
+    words_rdd = pre_process_rdd.flatMap(lambda line: line.split())
+    pairs_rdd = words_rdd.map(lambda word: (word, 1))
+    count_rdd = pairs_rdd.reduceByKey(lambda x, y: x+y)
+    return count_rdd
+
+
+if __name__ == "__main__":
+    text_rdd = sc.textFile("../data/word_count.txt")
+    counts = word_count(text_rdd)
+    print(counts.take(10))
+    # get the top n high frequency words
+    topn = counts.top(TOP_N, key=lambda x: x[1])
+    print(topn)
+
+
