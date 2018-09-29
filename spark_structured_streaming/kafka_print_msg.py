9ce99c0e09ad0b95ddf6f075a668d6650107ec88
buxizhizhoum <mapeaks@126.com>
2018-09-29 17:22:53 +0800

    Add kafka_print_msg.py

---
+++ spark_structured_streaming/kafka_print_msg.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+
+from pyspark.sql import SparkSession
+
+
+os.environ["PYSPARK_PYTHON"] = "/usr/bin/python3"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "/usr/bin/python3"
+
+# this is different with the DStream.
+# pay attention to the packages version, spark version should be 2.3.0
+os.environ['PYSPARK_SUBMIT_ARGS'] = \
+    '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 ' \
+    '--master local[2] ' \
+    'pyspark-shell'
+
+spark = SparkSession \
+        .builder \
+        .appName("KafkaStructuredStreaming") \
+        .getOrCreate()
+
+df = spark.readStream\
+    .format("kafka")\
+    .option("kafka.bootstrap.servers", "127.0.0.1:9092")\
+    .option("subscribe", "test")\
+    .load()
+
+df.printSchema()
+processed_df = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+output_df = processed_df.writeStream\
+    .format("console")\
+    .option("truncate", "false")\
+    .start()
+
+output_df.awaitTermination()
+
