0fcac16af3f5556934633f1fc08399c0993bc152
buxizhizhoum <mapeaks@126.com>
2018-10-16 09:00:17 +0800

    Add packages and modify document

--- spark_elasticsearch/spark_write_es.py
+++ spark_elasticsearch/spark_write_es.py
@@ -14,8 +14,11 @@ from pyspark import SparkConf
 
 os.environ["PYSPARK_PYTHON"] = "/usr/bin/python3"
 os.environ["PYSPARK_DRIVER_PYTHON"] = "/usr/bin/python3"
+# pay attention here, jars could be added at here
 os.environ['PYSPARK_SUBMIT_ARGS'] = \
-    '--jars /home/buxizhizhoum/2-Learning/pyspark_tutorial/jars/elasticsearch-hadoop-6.4.2/dist/elasticsearch-spark-20_2.11-6.4.2.jar pyspark-shell'
+    '--jars /home/buxizhizhoum/2-Learning/pyspark_tutorial/jars/elasticsearch-hadoop-6.4.2/dist/elasticsearch-spark-20_2.11-6.4.2.jar ' \
+    '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.1 ' \
+    'pyspark-shell'
 
 conf = SparkConf().setAppName("write_es").setMaster("local[2]")
 sc = SparkContext(conf=conf)
@@ -36,9 +39,16 @@ es_write_conf = {
 
 if __name__ == "__main__":
     data = [
-        {'1': '2', 'doc_id': 123},
-        {'2': '4', 'doc_id': 456},
-        {'3': '8', 'doc_id': 789}
+        {'1': '2', 'doc_id': 1},
+        {'2': '4', 'doc_id': 2},
+        {'3': '8', 'doc_id': 3},
+        {'4': '16', 'doc_id': 4},
+        {'5': '32', 'doc_id': 5},
+        {'6': '64', 'doc_id': 6},
+        {'7': '128', 'doc_id': 7},
+        {'8': '256', 'doc_id': 8},
+        {'9': '512', 'doc_id': 9},
+        {'10': '1024', 'doc_id': 10}
     ]
     rdd = sc.parallelize(data)
     rdd = rdd.map(lambda x: (x["doc_id"], json.dumps(x)))
